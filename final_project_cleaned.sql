6743c719641a672bf63d6819ea12bea4c9145b56
Xylia Rippy <rippyn@oregonstate.edu>
2024-03-11 14:48:27 -0700

    fixed table name

--- final_project_cleaned.sql
+++ final_project_cleaned.sql
@@ -382,7 +382,7 @@ on length(p.product_type) = x.max_type_length;
 
 --finds the non unique emails that already exist and emails that violate the code above
 SELECT email, COUNT(*)
-FROM test
+FROM user_test
 GROUP BY email
 HAVING COUNT(*) > 1;
 
@@ -398,36 +398,36 @@ WHERE email !~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$';
 WITH RankedEmails AS (
   SELECT usr_id, email,
          ROW_NUMBER() OVER(PARTITION BY email ORDER BY usr_id) AS rn
-  FROM test
+  FROM user_test
 )
-DELETE FROM test
+DELETE FROM user_test
 WHERE usr_id IN (SELECT usr_id FROM RankedEmails WHERE rn > 1);
 
 
 --ads a CONSTRAINT to the emails where all emails must be UNIQUE
-ALTER TABLE test ADD CONSTRAINT unique_usr_email UNIQUE (email);
+ALTER TABLE user_test ADD CONSTRAINT unique_usr_email UNIQUE (email);
 
 
 ---State/city name fixers
 
 --State fixer
 SELECT INITCAP(state_address) AS ProperState_address
-FROM test;
-UPDATE test
+FROM user_test;
+UPDATE user_test
 SET state_address = INITCAP(state_address);
-ALTER TABLE test ADD CONSTRAINT unique_state UNIQUE (state_address);
+ALTER TABLE user_test ADD CONSTRAINT unique_state UNIQUE (state_address);
 SELECT state_address, COUNT(*)
-FROM test
+FROM user_test
 GROUP BY state_address
 HAVING COUNT(*) > 1;
 --City fixer
 SELECT INITCAP(city_address) AS ProperCity_address
-FROM test;
-UPDATE test
+FROM user_test;
+UPDATE user_test
 SET city_address = INITCAP(city_address);
-ALTER TABLE test ADD CONSTRAINT unique_city UNIQUE (city_address);
+ALTER TABLE user_test ADD CONSTRAINT unique_city UNIQUE (city_address);
 SELECT city_address, COUNT(*)
-FROM test
+FROM user_test
 GROUP BY city_address
 HAVING COUNT(*) > 1;
 
@@ -437,7 +437,7 @@ HAVING COUNT(*) > 1;
 -- changes data FROM first_name to ProperFirstName and last_name to ProperLastName
 SELECT  INITCAP(first_name) as ProperFirstName, 
         INITCAP(last_name) as ProperLastName
-FROM test;
+FROM user_test;
 
 
 
