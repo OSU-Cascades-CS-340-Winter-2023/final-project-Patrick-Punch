353ce121702a719d33699912a0d6c3b158a6d9f5
Xylia Rippy <rippyn@oregonstate.edu>
2024-03-14 13:49:07 -0700

    fixed trigger systemgit pull origin maingit pull origin main!

--- (7)create_triggers.sql
+++ (7)create_triggers.sql
@@ -14,11 +14,23 @@ CREATE TABLE item_archive
 CREATE OR REPLACE FUNCTION item_update_trigger()
 RETURNS TRIGGER AS 
 $$
+CREATE OR REPLACE FUNCTION item_update_trigger()
+RETURNS TRIGGER AS 
+$$
 BEGIN
-    INSERT INTO item_archive
-    SELECT OLD.item_id, OLD.item_type, OLD.item_name, OLD.item_description, OLD.item_price, OLD.item_picture, NEW.item_price, CURRENT_DATE;
+--added the item_archive table vales in () to the insert into.
+    INSERT INTO item_archive(item_id, item_type, item_name, item_description, item_price, item_picture, date_updated)
+--changed select to values  to make it select values
+--also changed NEW. to OLD.
+    VALUES (OLD.item_id, OLD.item_type, OLD.item_name, OLD.item_description, OLD.item_price, OLD.item_picture, OLD.item_price, CURRENT_DATE);
     RETURN NEW;
 END;
+$$ LANGUAGE plpgsql;
+
+/*The INSERT INTO statement correctly lists all the target columns.
+The VALUES clause is used with OLD.* columns to get the values before the update and CURRENT_DATE to record the update time.
+The NEW.item_price was removed, and NEW.item_price doesn't have a corresponding column in the item_archive table. (changed to OLD.item_price)
+*/
 $$ LANGUAGE plpgsql;
 
 create trigger ia_update
