d1bb677b8084a948cf89203d9ca49987f187795f
Xylia Rippy <rippyn@oregonstate.edu>
2024-03-14 12:36:56 -0700

    added some more data fixing

--- (5)clean_data.sql
+++ (5)clean_data.sql
@@ -6,13 +6,11 @@ SELECT email, COUNT(*)
 FROM user_test
 GROUP BY email
 HAVING COUNT(*) > 1;
-go;
 --Selects FROM the emails we have and checks to see 
 --if the start has proper values then checks after the @ symbol
 --!~* operator is used for case-insensitive negation of a regular expression match
 DELETE from user_test
 WHERE email !~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$';
-go;
 -- Temporarily remove the unique constraint if necessary
 -- ALTER TABLE test DROP CONSTRAINT IF EXISTS unique_usr_email;
 WITH RankedEmails AS
@@ -23,15 +21,14 @@ WITH RankedEmails AS
   )
 DELETE FROM user_test
 WHERE usr_id IN (SELECT usr_id FROM RankedEmails WHERE rn > 1);
-go;
 --adds a CONSTRAINT to the emails where all emails must be UNIQUE
 ALTER TABLE user_test ADD CONSTRAINT unique_usr_email UNIQUE (email);
+go;
 
 
 ---State/city name fixers
 Select INITCAP(state_address) as ProperState_address,
       INITCAP(city_address) as ProperCity_address
-go;
 from user_test;
 set state_address = INITCAP(state_address),
     city_address =  INITCAP(city_address);
@@ -50,7 +47,18 @@ go;
 SELECT  INITCAP(first_name) as ProperFirstName, 
         INITCAP(last_name) as ProperLastName
 FROM user_test;
-go;
+--Changes the names to be gramatically correct
 Update user_test
 set first_name = INITCAP(first_name),
     last_name = INITCAP(last_name);
+go;
+/* Finds products with no id and removes it
+later update: make it skim all missing data values to then remove any row that
+has missing data.
+*/
+Select product_id
+from products_test
+where product_id = ' 'OR product_id IS NULL;
+
+DELETE from products_test
+where product_id = ' 'OR product_id IS NULL;
