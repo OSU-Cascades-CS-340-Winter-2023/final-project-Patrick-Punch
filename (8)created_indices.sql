df8650b54cc1172450fd53250547313275781296
Xylia Rippy <rippyn@oregonstate.edu>
2024-03-14 17:25:20 -0700

    editied some indices and changed some

--- (8)created_indices.sql
+++ (8)created_indices.sql
@@ -12,18 +12,18 @@ of _usr_id after the main part of the index name indicates that the index is
 built on the usr_id column of the user_interests table.
 */
 --user 
-CREATE INDEX idx_user_interests_usr_id ON user_interests(usr_id);
+CREATE INDEX idx_user_interests_usr_id ON usr_interests(usr_id);
 CREATE INDEX idx_user_item_preference_usr_id ON user_item_preference(usr_id);
 CREATE INDEX idx_user_company_preference_usr_id ON user_company_preference(usr_id);
-CREATE INDEX idx_user_location_preference_usr_id ON user_location_preference(usr_id);
+CREATE INDEX idx_user_location_preference_usr_city_state_zip ON user_location_preference(usr_city, usr_state, usr_zip);
 CREATE INDEX idx_user_company_review_usr_id ON user_company_review(usr_id);
 --item and service data
-CREATE INDEX idx_item_item_id ON item(item_id);
-CREATE INDEX idx_services_services_id ON services(services_id);
-CREATE INDEX idx_products_product_id ON products(product_id);
---company data
-CREATE INDEX idx_company_location_company_id ON company_location(company_id);
-CREATE INDEX idx_company_item_company_id ON company_item(company_id);
-CREATE INDEX idx_user_company_review_company_id ON user_company_review(company_id);
-CREATE INDEX idx_company_transaction_company_id ON company_transaction(company_id);
-CREATE INDEX idx_user_checkin_company_id ON user_checkin(company_id);
+CREATE INDEX idx_item_item_type ON item(item_type);
+CREATE INDEX idx_services_services_brand ON services(services_brand);
+CREATE INDEX idx_products_product_typebrand ON products(product_type, product_brand);
+-- --company data
+-- CREATE INDEX idx_company_location_company_id ON company_location(company_location);
+-- CREATE INDEX idx_company_item_company_id ON company_item(company_id);
+-- CREATE INDEX idx_user_company_review_company_id ON user_company_review(company_id);
+-- CREATE INDEX idx_company_transaction_company_id ON company_transaction(company_id);
+-- CREATE INDEX idx_user_checkin_company_id ON user_checkin(company_id);
